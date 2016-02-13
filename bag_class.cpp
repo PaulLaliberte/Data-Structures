554501e1f46656ee22819ec7cf2e5ef8bbc43413
PaulLaliberte <pala5552@colorado.edu>
2016-02-13 14:38:34 -0700

    Create bag_class.cpp

---
+++ bag_class.cpp
@@ -0,0 +1,46 @@
+```cpp
+//
+//  bag_class.cpp
+//  ADT Bag
+//
+//  Created by Paul Laliberte on 7/7/15.
+//  Copyright (c) 2015 Paul Laliberte. All rights reserved.
+//
+
+#include "bag.h"
+#include <iostream>
+
+Bag::Bag() //Constructor
+{
+    count = 0;
+}
+
+//Bag::Bag(int a) //*
+//{
+//    count = 0;
+//}
+
+void Bag::clear() //clear the bag
+{
+    count = 0;
+}
+
+bool Bag::insert(bag_type value) //Place a value in Bag
+{
+    bool reply;
+    if(count < 100)
+    {
+        data[count] = value;
+        reply = true;
+        count++;
+    }
+    else
+        reply = false;
+    return reply;
+}
+
+int Bag::size() //How many elements in bag?
+{
+    return count;
+}
+```
