01c8798b9109ad2c1ca5dea02fdb6fd4fe6d2884
PaulLaliberte <pala5552@colorado.edu>
2016-02-13 14:44:49 -0700

    Create (template class) magicbox.h

---
+++ (template class) magicbox.h
@@ -0,0 +1,29 @@
+```cpp
+//
+//  MagicBox.h
+//  Class_Template
+//
+//  Created by Paul Laliberte on 7/11/15.
+//  Copyright (c) 2015 Paul Laliberte. All rights reserved.
+//
+
+#ifndef MagicBox_h
+#define MagicBox_h
+#include "box.h"
+
+template <class itemtype>
+class magicbox : public hopefulbox<itemtype>
+{
+private:
+    bool firstitemstored;
+    
+public:
+    magicbox();
+    magicbox(const itemtype& theitem);
+    void setitem(const itemtype& theitem);
+
+};
+
+
+#endif
+```
