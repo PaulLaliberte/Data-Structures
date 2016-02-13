947555692497fc067638a1f30436485900fc2891
PaulLaliberte <pala5552@colorado.edu>
2016-02-13 15:06:25 -0700

    Create StackInterface.h

---
+++ StackInterface.h
@@ -0,0 +1,26 @@
+```cpp
+//
+//  StackInterface.h
+//  Chapter6_Stack
+//
+//  Created by Paul Laliberte on 7/23/15.
+//  Copyright (c) 2015 Paul Laliberte. All rights reserved.
+
+
+#ifndef Chapter6_Stack_StackInterface_h
+#define Chapter6_Stack_StackInterface_h
+
+template<class ItemType>
+class StackInterface
+{
+public:
+    virtual bool isEmpty() const = 0;
+    
+    virtual bool push(const ItemType& newEntry) = 0;
+    
+    virtual bool pop() = 0;
+    
+    virtual void peek() const = 0;
+};
+
+#endif
