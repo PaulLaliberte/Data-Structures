9eaf48c4445086340f4923847fefa1b2046725b9
PaulLaliberte <pala5552@colorado.edu>
2016-02-13 15:08:29 -0700

    Create stack_test.cpp

---
+++ stack_test.cpp
@@ -0,0 +1,47 @@
+```cpp
+#include "stack.h"
+#include "stack.cpp"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+
+int main()
+{
+    stack<int> intStack;
+    intStack.push(100);
+    intStack.pop();
+    intStack.push(12);
+    intStack.push(10);
+    intStack.peek();
+    intStack.pop();
+    intStack.peek();
+    
+    cout << endl;
+    cout << endl;
+    
+    stack<string> stringStack;
+    stringStack.push("Hello");
+    stringStack.push("everyone");
+    stringStack.pop();
+    stringStack.peek();
+    stringStack.pop();
+    stringStack.peek();
+    stringStack.peek();
+    stringStack.pop();
+    
+    cout << endl;
+    cout << endl;
+    
+    stack<double> doubleStack;
+    doubleStack.push(33.3);
+    doubleStack.peek();
+    doubleStack.push(22.5);
+    doubleStack.peek();
+    
+    //Cannot add because of bool, switch to itemtype to add. 
+    
+}
+```
