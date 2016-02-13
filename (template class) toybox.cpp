55701b7662e3f41702fe96a580e5d45cfc3ccf42
PaulLaliberte <pala5552@colorado.edu>
2016-02-13 14:45:42 -0700

    Create (template class) toybox.cpp

---
+++ (template class) toybox.cpp
@@ -0,0 +1,37 @@
+```cpp
+#include "ToyBox.h"
+#include <iostream>
+
+
+//Defualt Constructor
+template<class itemtype>
+ToyBox<itemtype>::ToyBox()
+{
+    hopefulbox<itemtype>();
+    boxColor = BLACK;
+}
+
+
+//Constructor
+template<class itemtype>
+ToyBox<itemtype>::ToyBox(const Color& theColor)
+{
+    hopefulbox<itemtype>();
+    boxColor = theColor;
+}
+
+
+template<class itemtype>
+ToyBox<itemtype>::ToyBox(const itemtype& theitem, const Color& theColor)
+{
+    hopefulbox<itemtype>();
+    hopefulbox<itemtype>::setitem(theitem);
+    boxColor = theColor;
+}
+
+template<class itemtype>
+Color ToyBox<itemtype>::getColor() const
+{
+    return boxColor;
+}
+```
