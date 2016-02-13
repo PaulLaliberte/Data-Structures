58ef2694ac29d3eb5b4bf1d535494cc40a7c1550
PaulLaliberte <pala5552@colorado.edu>
2016-02-13 14:46:08 -0700

    Create (template class) magicbox.cpp

---
+++ (template class) magicbox.cpp
@@ -0,0 +1,32 @@
+```cpp
+#include "MagicBox.h"
+
+
+template<class itemtype>
+magicbox<itemtype>::magicbox()
+{
+    hopefulbox<itemtype>();
+    firstitemstored = false;
+}
+
+
+template<class itemtype>
+magicbox<itemtype>::magicbox(const itemtype& theitem)
+{
+    firstitemstored = false;
+    setitem(theitem);
+}
+
+
+template<class itemtype>
+void magicbox<itemtype>::setitem(const itemtype& theitem)
+{
+    if (!firstitemstored)
+    {
+        hopefulbox<itemtype>::setitem(theitem);
+        firstitemstored = true;
+    }
+}
+
+
+```
