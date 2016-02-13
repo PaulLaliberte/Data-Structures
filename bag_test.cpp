6e6e6ff69e2891061362d1c9cbdc44b15fbe1adc
PaulLaliberte <pala5552@colorado.edu>
2016-02-13 14:38:58 -0700

    Create bag_test.cpp

---
+++ bag_test.cpp
@@ -0,0 +1,34 @@
+```cpp
+//
+//  bag_test.cpp
+//  ADT Bag
+//
+//  Created by Paul Laliberte on 7/7/15.
+//  Copyright (c) 2015 Paul Laliberte. All rights reserved.
+
+#include <iostream>
+#include <cstdlib>
+#include "bag.h"
+
+using namespace std;
+
+int main()
+{
+    Bag b;
+//  Bag a(10); //*
+    bag_type value;
+    
+    cout << "Let's create a bag with only one element: " << endl;
+    b.insert(5); //inserts one element into the bag
+    cout << b.size() << " element(s) in bag the bag." << endl;
+    b.clear();
+    cout << b.size() << " element(s) in the bag after b.clear()." << endl;
+    
+    do {
+        value = rand() % 6 + 1;
+    } while(b.insert(value));
+    cout << b.size() << " element(s) in the bag." << endl;
+    b.clear();
+    cout << b.size() << " element(s) in the bag after b.clear()." << endl;
+}
+```
