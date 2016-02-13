103d632fa3184f790769f4bc84b4d0b23451eebd
PaulLaliberte <pala5552@colorado.edu>
2016-02-13 15:09:17 -0700

    Create array_stack.h

---
+++ array_stack.h
@@ -0,0 +1,30 @@
+```
+//
+//  ArrayBag.h
+//  Chapter7_ArrayStack
+//
+//  Created by Paul Laliberte on 7/28/15.
+//  Copyright (c) 2015 Paul Laliberte. All rights reserved.
+//
+
+#ifndef __Chapter7_ArrayStack__ArrayBag__
+#define __Chapter7_ArrayStack__ArrayBag__
+
+template <class type>
+class ArrayStack {
+private:
+    static const int MAX_CAPACITY = 12;
+    int top;
+    type item[MAX_CAPACITY];
+    
+public:
+    ArrayStack();
+    bool isEmpty();
+    bool push(const type& aItem);
+    bool pop();
+    void peek();
+};
+
+#endif /* defined(__Chapter7_ArrayStack__ArrayBag__) */
+
+```
