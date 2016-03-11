6f9cb8d89f4f47e2fd8e4a1d78e9bf3a6515265c
PaulLaliberte <pala5552@colorado.edu>
2016-03-10 17:33:15 -0700

    added in counter for total moves.

--- TowerofHanoi.cpp
+++ TowerofHanoi.cpp
@@ -11,17 +11,18 @@
 #include <iostream>
 using namespace std;
 
-void solveTowersproblem(int count, char source, char destination, char spare)
+void solveTowersproblem(int count, char source, char destination, char spare, int *move)
 {
     if (count == 1)
     {
         cout << "Move top disk from pole " << source << " to pole " << destination << endl;
+        *move += 1;
     }
     else
     {
-        solveTowersproblem(count - 1, source, spare, destination);
-        solveTowersproblem(1, source, destination, spare);
-        solveTowersproblem(count - 1, spare, destination, source);
+        solveTowersproblem(count - 1, source, spare, destination, move);
+        solveTowersproblem(1, source, destination, spare, move);
+        solveTowersproblem(count - 1, spare, destination, source, move);
     }
 }
 
@@ -31,10 +32,14 @@ int main()
     cout << "How many disc(s) should we play the game with? ";
     cin >> discs;
     
+    int number_of_moves;
+    
+    
     char towerA, towerB, towerC;
     towerA = 'A';
     towerB = 'B';
     towerC = 'C';
-    solveTowersproblem(discs, towerA, towerB, towerC);
+    solveTowersproblem(discs, towerA, towerB, towerC, &number_of_moves);
+    cout << "Total number of moves: " << number_of_moves << endl;
 }
 
